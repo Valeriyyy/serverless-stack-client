a4d169c124ca600ca3676cbfeeddf775ef4bb54c
Valeriyyy <valeriykutsar18@gmail.com>
2021-07-05 19:25:57 -0700

    created button to indicate logging in and loading

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../../libs/contextLib";
+import LoaderButton from "../../components/LoaderButton/LoaderButton";
 import "./Login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const { userHasAuthenticated } = useAppContext();
   const history = useHistory();
 
@@ -18,13 +19,14 @@ export default function Login() {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
+    setIsLoading(true);
     try {
       await Auth.signIn(email, password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
       alert(e.message);
+      setIsLoading(false);
     }
   }
 
@@ -49,9 +51,15 @@ export default function Login() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
+        <LoaderButton
+          block
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </Form>
     </div>
   );
