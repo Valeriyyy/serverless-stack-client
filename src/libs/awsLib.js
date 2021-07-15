b1cfcd52c4e3f4ab83fb5c767622f937e8bdb545
Valeriyyy <valeriykutsar18@gmail.com>
2021-07-14 22:00:31 -0700

    added displaying all users notes

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -1,9 +1,9 @@
-import { Storate } from "aws-amplify";
+import { Storage } from "aws-amplify";
 
 export async function s3Upload(file) {
   const fileName = `${Date.now()}-${file.name}`;
 
-  const stored = await Storage.vaul.put(filename, file, {
+  const stored = await Storage.vaul.put(fileName, file, {
     contentType: file.type,
   });
 
